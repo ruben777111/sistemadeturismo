ec4d4475bbd64177d443e7d4b2bb5d1930c1b4cf
Copa <lorenzero@outlook.com>
2022-04-10 20:42:30 -0400

    ultimo commit

--- proyturismo/urls.py
+++ proyturismo/urls.py
@@ -13,8 +13,8 @@ urlpatterns=[
     path('destinoturistico/crear',views.creardestinoturistico,name='creardestinoturistico'),
     path('destinoturistico/editar',views.editardestinoturistico,name='editardestinoturistico'),
     
-  
+
     path('boleto',views.boleto,name='boleto'),
 
- 
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
