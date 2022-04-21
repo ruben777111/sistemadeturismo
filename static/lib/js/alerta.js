e695baed18f4296cd2ae778530cdd325638740bd
Copa <lorenzero@outlook.com>
2022-04-21 11:11:04 -0400

    tablas

---
+++ static/lib/js/alerta.js
@@ -0,0 +1,67 @@
+Swal.fire({
+	title: "Datos Incorrectos",
+	text: "Ingrese los datos nuevamente",
+	type: "warning",
+	// showCancelButton: false,
+	// confirmButtonColor: "#007bff",
+	// confirmButtonText: "Aceptar",
+	// cancelButtonText: "No, cancel plx!",
+	// cancelButtonColor: "#33A2FF",
+	// closeOnConfirm: false,
+	// closeOnCancel: false,
+	// customClass: "Custom_Cancel",
+	icon: 'error',
+	// confirmButtonText:
+	// footer:
+	width: '39%',
+	// padding: '1rem',
+	background: '#f7fff7',
+	// grow:
+	// backdrop:
+	// timer:
+	// timerProgressBar:
+	// toast:
+	// position:
+	allowOutsideClick: false,
+	// allowEscapeKey:
+	// allowEnterKey:
+	// stopKeydownPropagation:
+
+	// input:
+	// inputPlaceholder:
+	// inputValue:
+	// inputOptions:
+
+	//  customClass:
+	// 	container:
+	// 	popup:
+	// 	header:
+	// 	title:
+	// 	closeButton:
+	// 	icon:
+	// 	image:
+	// 	content:
+	// 	input:
+	// 	actions:
+		confirmButton: 'Aceptar',
+	// 	cancelButton:
+	// 	footer:
+
+	// showConfirmButton:
+	confirmButtonColor: '#007bff',
+	// confirmButtonAriaLabel:
+
+	// showCancelButton:
+	// cancelButtonText:
+	// cancelButtonColor:
+	// cancelButtonAriaLabel:
+
+	// buttonsStyling:
+	// showCloseButton:
+	// closeButtonAriaLabel:
+
+	// imageUrl:
+	// imageWidth:
+	// imageHeight:
+	// imageAlt:
+});
