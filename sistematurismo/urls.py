ae1fa7c1fbce61c1b0fb744a7f9b7873ff6cb681
ruben777111 <ruben639mt@gmail.com>
2022-04-23 22:07:14 -0400

    actualizado

--- sistematurismo/urls.py
+++ sistematurismo/urls.py
@@ -20,5 +20,5 @@ from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('proyturismo.urls')),
-    path('', include('paginaweb.urls')),
+    
 ]
